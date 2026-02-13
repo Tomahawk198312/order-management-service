189fe78541a35546055ed72a55407b4e94172763
Tomahawk198312 <129873302+Tomahawk198312@users.noreply.github.com>
2026-02-13 20:42:43 +0500

    start commit

---
+++ src/main/java/org/specializedclothing/order_management_service/SpecializedClothingOrderManagerApplication.java
@@ -0,0 +1,13 @@
+package org.specializedclothing.order_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecializedClothingOrderManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecializedClothingOrderManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/specializedclothing/order_management_service/SpecializedClothingOrderManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.specializedclothing.order_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpecializedClothingOrderManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
